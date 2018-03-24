688ab1ea3dfcf3c4242f0007cc9c409be6fc2663
Arsyel <ahmad.fire@gmail.com>
2018-03-24 19:08:12 +0700

    add deployment dir

---
+++ Deployment/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1521891813,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bg_title-sheet0.png",
+		"images/btn_play-sheet0.png",
+		"images/bg_picklevel-sheet0.png",
+		"images/btn_back-sheet0.png",
+		"images/btn_exit-sheet0.png",
+		"images/btn_type-sheet0.png",
+		"images/camera-sheet0.png",
+		"images/global-sheet0.png",
+		"images/win_about-sheet0.png",
+		"images/bg_theme-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
